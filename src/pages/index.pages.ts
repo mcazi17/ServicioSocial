a0f48466416004b4e809a7c141d8f66fa06c4d9b
Emiliano <emiliano_rt@hotmail.com>
2018-05-15 20:25:40 -0700

    add video page to index.pages.ts

--- src/pages/index.pages.ts
+++ src/pages/index.pages.ts
@@ -4,5 +4,6 @@ export { MyApp } from '../app/app.component';
 export { HomePage } from '../pages/home/home';
 export { ListPage } from '../pages/list/list';
 export { InventoryPage } from '../pages/inventory/inventory';
+export { VideosPage } from '../pages/videos/videos';
 export { ModalOptions } from '../pages/modal-options/modal';
 export { SlidesPage } from '../pages/slides/slides';
