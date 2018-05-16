bc09c8701e447d199e41a1800641629bde5a5b35
Felipe Ortega Casillas <felipeorcas@gmail.com>
2018-05-15 20:09:13 -0700

    Modal-options finished

--- src/pages/modal-options/modal.ts
+++ src/pages/modal-options/modal.ts
@@ -24,32 +24,46 @@ export class ModalOptions {
 
   ionViewDidLoad() {
     this.item = this.navParams.get('item')
-    this.arrayName = this.item.arrayName
+    this.arrayName = this.item.array_name
     this.name = this.item.name;
     this.price_s = this.item.price_s;
     this.price_m = this.item.price_m;
     this.price_l = this.item.price_l;
     console.log("item.name:" + this.name);
-    
-    this.selectImage(this.arrayName);
+
+    switch (this.arrayName) {
+      case "coffees":
+      case "others":
+      case "teas":
+        this.image_s = "././assets/imgs/CalienteS.jpg";
+        this.image_m = "././assets/imgs/CalienteM.jpg";
+        this.image_l = "././assets/imgs/CalienteL.jpg";
+        console.log("Image_s: "+this.image_s);
+        break;
+      case "cold_drinks":
+        this.image_s = "././assets/imgs/FriaS.jpg";
+        this.image_m = "././assets/imgs/FriaM.jpg";
+        this.image_l = "././assets/imgs/FriaL.jpg";
+        break;
+    }
   }
 
   selectImage(arrayName: string) {
     switch (arrayName) {
       case "coffees":
-        this.image_s = "../../assets/imgs/CalienteS";
-        this.image_m = "../../assets/imgs/CalienteM";
-        this.image_l = "../../assets/imgs/CalienteL";
+        this.image_s = "https://ia.media-imdb.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_UX182_CR0,0,182,268_AL_.jpg";
+        this.image_m = "../../assets/imgs/CalienteM.jpg";
+        this.image_l = "../../assets/imgs/CalienteL.jpg";
         break;
       case "teas":
-        this.image_s = "../../assets/imgs/CalienteS";
-        this.image_m = "../../assets/imgs/CalienteM";
-        this.image_l = "../../assets/imgs/CalienteL";
+        this.image_s = "../../assets/imgs/CalienteS.jpg";
+        this.image_m = "../../assets/imgs/CalienteM.jpg";
+        this.image_l = "../../assets/imgs/CalienteL.jpg";
         break;
       case "cold_drinks":
-        this.image_s = "../../assets/imgs/FriaS";
-        this.image_m = "../../assets/imgs/FriaM";
-        this.image_l = "../../assets/imgs/FriaL";
+        this.image_s = "../../assets/imgs/FriaS.jpg";
+        this.image_m = "../../assets/imgs/FriaM.jpg";
+        this.image_l = "../../assets/imgs/FriaL.jpg";
         break;
     }
   }
@@ -58,6 +72,6 @@ export class ModalOptions {
     this.viewCtrl.dismiss();
   }
   selectOption(price: string, size: Size) {
-
+    console.log("CLICK");
   }
 }
